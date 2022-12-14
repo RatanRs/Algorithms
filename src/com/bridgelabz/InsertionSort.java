b1ba623a00a199f0e6cced0883eca40d1b0c89a9
9663461085 <ratansollapure16310@gmail.com>
2022-12-14 10:00:57 +0530

    Adde:Printing elements in sorting order

---
+++ src/com/bridgelabz/InsertionSort.java
@@ -0,0 +1,41 @@
+package com.bridgelabz;
+
+public class InsertionSort {
+	/* Function to sort array using insertion sort */
+	void sort(int arr[]) {
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+
+			/*
+			 * Move elements of arr[0..i-1], that are greater than key, to one position
+			 * ahead of their current position
+			 */
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	/* A utility function to print array of size n */
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+	}
+
+	public static void main(String args[]) {
+		System.out.println("The elements before sorting: ");
+		System.out.println("12 11  13  5  6 ");
+		int arr[] = { 12, 11, 13, 5, 6 };
+		InsertionSort input = new InsertionSort();
+		System.out.println("The elements after sorting");
+		input.sort(arr);
+        printArray(arr);
+	}
+}
